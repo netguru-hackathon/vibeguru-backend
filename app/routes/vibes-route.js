359a891f09e9c2af92f016a270fa04dd82211676
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-06-24 13:56:54 +0200

    Add a logic inside a route

--- app/routes/vibes-route.js
+++ app/routes/vibes-route.js
@@ -9,7 +9,23 @@ const Vibe = require('./../models/vibe');
 router.route('/')
 
   .post((req, res) => {
+    const vibe = new Vibe({image: req.body.image});
 
+    vibe.save(function(error) {
+      if(error) throw err;
+
+      Project.find({ url: req.body.url}, function (error, project) {
+        if(!project) {
+          project = new Project();
+        }
+        project.name = req.body.name;
+        project.url = req.body.url;
+        project.vibes.push(vibe._id);
+        project.save(function(error) {
+          if (error) throw error;
+        });
+      });
+    });
   })
 
   .get((req, res) => {

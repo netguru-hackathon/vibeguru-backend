99afea1c9338b68d2460030c3b1c02bd808fea88
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-06-24 14:52:13 +0200

    find -> findOne

--- app/routes/vibes-route.js
+++ app/routes/vibes-route.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const resUtil = require('./../utils/res-util');
 
 const Vibe = require('./../models/vibe');
+const Project = require('./../models/project');
 
 router.route('/')
 
@@ -11,24 +12,33 @@ router.route('/')
     const vibe = new Vibe({image: req.body.image});
 
     vibe.save(function(error) {
-      if(error) throw err;
+      if (error) throw err;
+      const repo_url = req.body.repo_url;
 
-      Project.find({ url: req.body.url }, function (error, project) {
-        if(!project) {
-          project = new Project();
-        }
-        project.name = req.body.name;
-        project.url = req.body.url;
+      Project
+      .findOne({ url: repo_url })
+      .exec(function (error, project) {
+        if(!project) project = new Project;
+
+        project.name = repo_url;
+        project.url = repo_url;
         project.vibes.push(vibe._id);
+
         project.save(function(error) {
           if (error) throw error;
+          resUtil.success(res);
         });
       });
     });
   })
 
   .get((req, res) => {
-
+    Project
+    .find()
+    .populate('vibes')
+    .exec((err, projects) => {
+      return resUtil.success(res, projects);
+    });
   });
 
 module.exports = router;
